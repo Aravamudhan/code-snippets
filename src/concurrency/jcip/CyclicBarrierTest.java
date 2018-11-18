8f34113c38bfd5d273a12d13cff61055d539702f
aravamudhan <aravamudhan.a@gmail.com>
2018-11-18 12:13:49 +0530

    CyclicBarrier, Comments in ProducerConsumer, formatting

---
+++ src/concurrency/jcip/CyclicBarrierTest.java
@@ -0,0 +1,111 @@
+package concurrency.jcip;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Show an example of using the CyclicBarrier
+ * 
+ * @author amudhan
+ *
+ */
+public class CyclicBarrierTest {
+
+  public static void main(String[] args) {
+    // Create an object for the outer class
+    CyclicBarrierTest cbTest = new CyclicBarrierTest();
+    // Use the outer class object to create an object for the inner class and
+    CyclicBarrierContainer cbContainer = cbTest.new CyclicBarrierContainer();
+    cbContainer.runSimulation(5, 3);
+  }
+
+  private class CyclicBarrierContainer {
+
+    private CyclicBarrier cyclicBarrier;
+    private List<List<Integer>> partialResults = Collections.synchronizedList(new ArrayList<>());
+    private Random random = new Random();
+    private int NUM_PARTIAL_RESULTS;
+    private int NUM_WORKERS;
+
+    public void runSimulation(int numWorkers, int numberOfPartialResults) {
+      NUM_PARTIAL_RESULTS = numberOfPartialResults;
+      NUM_WORKERS = numWorkers;
+      cyclicBarrier = new CyclicBarrier(NUM_WORKERS, new AggregatorThread());
+
+      System.out.println("Spawning " + NUM_WORKERS + " worker threads to compute "
+          + NUM_PARTIAL_RESULTS + " partial results each");
+
+      // Creating worker threads
+      for (int i = 0; i < NUM_WORKERS; i++) {
+        Thread worker = new Thread(new NumberCruncherThread());
+        worker.setName("Thread " + i);
+        worker.start();
+      }
+    }
+
+    /**
+     * This is the worker thread
+     * 
+     * @author amudhan
+     *
+     */
+    private class NumberCruncherThread implements Runnable {
+      @Override
+      public void run() {
+        String thisThreadName = Thread.currentThread().getName();
+        List<Integer> partialResult = new ArrayList<>();
+
+        // Crunch some numbers and store the partial result
+        for (int i = 0; i < NUM_PARTIAL_RESULTS; i++) {
+          Integer num = random.nextInt(10);
+          System.out.println(thisThreadName + ": Crunching some numbers! Final result - " + num);
+          partialResult.add(num);
+        }
+        partialResults.add(partialResult);
+
+        try {
+          System.out.println(thisThreadName + " waiting for others to reach barrier.");
+          cyclicBarrier.await();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    /**
+     * This Runnable gets executed after all the threads have reached the barrier
+     * 
+     * @author amudhan
+     *
+     */
+    private class AggregatorThread implements Runnable {
+      @Override
+      public void run() {
+        // Any of the worker threads might execute this
+        String thisThreadName = Thread.currentThread().getName();
+
+        System.out.println(thisThreadName + ": Computing sum of " + NUM_WORKERS
+            + " workers, having " + NUM_PARTIAL_RESULTS + " results each.");
+        int sum = 0;
+        // Aggregate all the results
+        for (List<Integer> threadResult : partialResults) {
+          System.out.print("Adding ");
+          for (Integer partialResult : threadResult) {
+            System.out.print(partialResult + " ");
+            sum += partialResult;
+          }
+          System.out.println();
+        }
+        System.out.println(thisThreadName + ": Final result = " + sum);
+      }
+    }
+
+  }
+
+}
--- src/concurrency/semaphores/ProducerConsumer.java
+++ src/concurrency/semaphores/ProducerConsumer.java
@@ -3,113 +3,120 @@ package concurrency.semaphores;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-/* How it works
- * 1. Producer creates a resource, Consumer consumes the resource.
- * 2. Consumer can not consume when there are nothing is produced.
+/*
+ * How it works 1. Producer creates a resource, Consumer consumes the resource. 2. Consumer can not
+ * consume when there are nothing is produced.
  */
 /*
- * 'acquire' or Wait - decrementing the value of a semaphore.
- *   If the value becomes negative the thread that decrements waits
- *   until it becomes non negative again.
- * 'release()'or Signal - increasing the value of a semaphore.
- *   One and only way to increment a semaphore value so that the blocked
- *   threads(i.e.)threads that wait for this value to become non negative,
- *   get activated.
+ * 'acquire' or Wait - decrementing the value of a semaphore. If the value becomes negative the
+ * thread that decrements waits until it becomes non negative again. 'release()'or Signal -
+ * increasing the value of a semaphore. One and only way to increment a semaphore value so that the
+ * blocked threads(i.e.)threads that wait for this value to become non negative, get activated.
  */
 public class ProducerConsumer {
 
-	/*
-	 * This is the value(resource) that gets updated in the Producer thread and
-	 * read in the Consumer thread.
-	 */
-	static String event = "";
-	/*
-	 * Only by acquiring the lock variable Producer or Consumer can access the
-	 * event variable.
-	 */
-	/*
-	 * This variable is equivalent of a mutex.
-	 */
-	static Semaphore lock = new Semaphore(1);
-	/*
-	 * The purpose of this variable to let the Consumer know that the event
-	 * variable has a new value. When this value is accessed for the first time,
-	 * it is incremented by the Producer. Hence it is initialized with zero.
-	 */
-	static Semaphore available = new Semaphore(0);
+  /*
+   * This is the value(resource) that gets updated in the Producer thread and read in the Consumer
+   * thread.
+   */
+  static String event = "";
+  /*
+   * Only by acquiring the lock variable Producer or Consumer can access the event variable.
+   */
+  /*
+   * This variable is equivalent of a mutex.
+   */
+  static Semaphore eventLock = new Semaphore(1);
+  /*
+   * The purpose of this variable to let the Consumer know that the event variable has a new value.
+   * When this value is accessed for the first time, it is incremented by the Producer. Hence it is
+   * initialized with zero. This variable makes sure that the consumer waits until event becomes
+   * available for consumption. This should never be released in the consumer thread. It must only
+   * be acquired in the consumer thread
+   */
+  static Semaphore eventAvailable = new Semaphore(0);
 
-	public static void main(String[] args) {
-		/**
-		 * Starting producer and consumer threads.
-		 */
-		new Producer().start();
-		new Consumer().start();
-	}
+  public static void main(String[] args) {
+    /**
+     * Starting producer and consumer threads.
+     */
+    new Consumer().start();
+    try {
+      Thread.sleep(1500);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    new Producer().start();
+  }
 
 }
+
 
 class Producer extends Thread {
-	public void run() {
-		System.out.println("Inside producer");
-		Scanner in = new Scanner(System.in);
-		String input = null;
-		do {
-			System.out.println("Enter event name. Type no to stop.   ");
-			input = in.next();
-			try {
-				/*
-				 * Acquiring a semaphore variable named lock. acquire method
-				 * decreases the value of a semaphore. In this case that becomes
-				 * 0.
-				 */
-				ProducerConsumer.lock.acquire();
-				{
-					ProducerConsumer.event = input;
-					System.out.println("Produced " + ProducerConsumer.event);
-					/*
-					 * Once the operation is completed 'lock' is released so
-					 * that consumer can acquire it.
-					 */
-				}
-				ProducerConsumer.lock.release();
-				/*
-				 * Only if the semaphore variable 'available' is released,
-				 * (i.e.) becomes non negative, can the consumer access the
-				 * event variable.
-				 */
-				ProducerConsumer.available.release();
-				/*
-				 * Putting the current thread to sleep is mandatory. Otherwise
-				 * this thread will keep updating the event variable, but the
-				 * consumer thread wont get a chance to access the values.
-				 */
-				Producer.sleep(1);
-			} catch (InterruptedException e) {
-				System.out.println("Exception in the Producer "
-						+ e.getMessage());
-				e.printStackTrace();
-			}
-		} while (!input.equalsIgnoreCase("no"));
-		in.close();
-	}
+  public void run() {
+    System.out.println("Producer started.....");
+    Scanner in = new Scanner(System.in);
+    String input = null;
+    do {
+      System.out.println("Enter event name. Type no to stop.   ");
+      input = in.next();
+      try {
+        /*
+         * Acquiring a semaphore variable named lock. acquire method decreases the value of a
+         * semaphore. In this case that becomes 0.
+         */
+        ProducerConsumer.eventLock.acquire();
+        {
+          ProducerConsumer.event = input;
+          System.out.println("Produced " + ProducerConsumer.event);
+          /*
+           * Once the operation is completed 'lock' is released so that consumer can acquire it.
+           */
+        }
+        ProducerConsumer.eventLock.release();
+        /*
+         * Only if the semaphore variable 'eventAvailable' is released, (i.e.) becomes non negative,
+         * can the consumer thread access the event variable.
+         */
+        ProducerConsumer.eventAvailable.release();
+        /*
+         * Putting the current thread to sleep is mandatory. Otherwise this thread will keep
+         * updating the event variable, but the consumer thread wont get a chance to access the
+         * values.
+         */
+        Producer.sleep(1);
+      } catch (InterruptedException e) {
+        System.out.println("Exception in the Producer " + e.getMessage());
+        e.printStackTrace();
+      }
+    } while (!input.equalsIgnoreCase("no"));
+    in.close();
+  }
 }
 
+
 class Consumer extends Thread {
-	public void run() {
-		System.out.println("Inside consumer");
-		while (!ProducerConsumer.event.equalsIgnoreCase("no")) {
-			try {
-				ProducerConsumer.available.acquire();
-				ProducerConsumer.lock.acquire();
-				{
-					System.out.println("Consumed " + ProducerConsumer.event);
-				}
-				ProducerConsumer.lock.release();
-			} catch (InterruptedException e) {
-				System.out.println("Exception in the Consumer "
-						+ e.getMessage());
-				e.printStackTrace();
-			}
-		}
-	}
+  public void run() {
+    System.out.println("Consumer started.....");
+    while (!ProducerConsumer.event.equalsIgnoreCase("no")) {
+      try {
+        /**
+         * If the producer has not produced an event, then the eventAvailable will be 0. The
+         * consumer thread will wait until it becomes > 0
+         */
+        ProducerConsumer.eventAvailable.acquire();
+        /**
+         * Now lock is acquired on the event
+         */
+        ProducerConsumer.eventLock.acquire();
+        {
+          System.out.println("Consumed " + ProducerConsumer.event);
+        }
+        ProducerConsumer.eventLock.release();
+      } catch (InterruptedException e) {
+        System.out.println("Exception in the Consumer " + e.getMessage());
+        e.printStackTrace();
+      }
+    }
+  }
 }
